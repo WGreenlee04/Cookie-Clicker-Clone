81f7527cafa8c51d7bf32c787098ce2e4c4d8f55
wmgreenlee <wmgreenlee@PLD-LAB-400-23T.fayette.ketsds.net>
2019-03-26 09:46:53 -0400

    bla

--- src/gameFiles/MainGame.java
+++ src/gameFiles/MainGame.java
@@ -1,9 +1,11 @@
 package gameFiles;
 
-public class MainGame {
+import javax.swing.*;
+import java.util.*;
+
+public class MainGame extends JFrame{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 

b4b9879d51d69c01a7ce801167e0a77967f364eb
wmgreenlee <wmgreenlee@PLD-LAB-400-23T.fayette.ketsds.net>
2019-04-10 09:38:09 -0400

    FINALLY JACK

--- src/gameFiles/MainGame.java
+++ src/gameFiles/MainGame.java
@@ -3,6 +3,7 @@ package gameFiles;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
@@ -11,70 +12,77 @@ import java.io.*;
 import javax.imageio.*;
 import javax.swing.*;
 
-public class MainGame implements MouseListener {
+	public class MainGame implements MouseListener {
 
-	static final int JFRAME_HEIGHT = 1080;
-	static final int JFRAME_LENGTH = 1920;
+		static final int JFRAME_HEIGHT = 1080;
+		static final int JFRAME_LENGTH = 1920;
 
-	private int score;
-	private int score_multiplier;
-	private int baseScore;
-	private String dir;
+		static int score = 0;
+		private int score_multiplier;
 
-	public MainGame() {
-		score = 0;
-		score_multiplier = 1;
-		baseScore = 1;
-		dir = "H:/pics/Cookie.JFIF";
+		private static JFrame frame = new JFrame("Cookie Clicker");
+		private static JTextArea ccta = new JTextArea("Cookies: " + score);
+
+		public MainGame() {
+			score_multiplier = 1;
 	}
 
-    public static void main(String [] args) throws IOException {
+	public static void main(String [] args) throws IOException {
 
-        JFrame frame = new JFrame("Cookie Clicker");
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-
-        /**********************PROMPT FOR IMAGE DIRECTORY******************/
-        JFrame prompt=new JFrame("Where dat File?");
-        JPanel pPanel=new JPanel();
-        JLabel label2 = new JLabel();
-        pPanel.setLayout(new BorderLayout());
-        pPanel.add(label2, BorderLayout.WEST);
-        pPanel.addMouseListener(null);
-        prompt.add(pPanel);
-        /******************************************************************/
-
-        /***************ADD IMAGE AND CENTER WEST [LEFT]*******************/
-        BufferedImage cookimg = ImageIO.read(new File("H:/pics/Cookie.JFIF"));
-        JLabel label = new JLabel(new ImageIcon(cookimg));
-        panel.setLayout(new BorderLayout());
-        panel.add(label, BorderLayout.WEST);
-        panel.addMouseListener(null);
-        /*****************************************************************/
-
-        frame.add(panel);
-        label.setLocation(10,10);
-        frame.setSize(JFRAME_LENGTH, JFRAME_HEIGHT);
-        //frame.getContentPane().setBackground(Color.BLACK);
+	/**********************INSTANTIATE********************************/
+		frame.setLayout(new BorderLayout());
+		JPanel cookiepanel = new JPanel();
+		ccta.setFont(new Font("Comic Sans MS", Font.BOLD, 40));
+    /***************ADD IMAGE AND CENTER WEST [LEFT]*******************/
+		BufferedImage cookimg = ImageIO.read(new File("H:\\Pics\\Cookie.png"));
+		JLabel label = new JLabel(new ImageIcon(cookimg));
+		cookiepanel.setLayout(new BorderLayout());
+		cookiepanel.add(label, BorderLayout.WEST);
+    /*****************************************************************/
+		frame.add(ccta, BorderLayout.NORTH);
+		frame.add(cookiepanel);
+		label.setLocation(10,10);
+		frame.setSize(JFRAME_LENGTH, JFRAME_HEIGHT);
+		frame.getContentPane().setBackground(Color.WHITE);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-    }
 
-    public void mouseClicked(MouseEvent e) {
-    	int vertcent = JFRAME_HEIGHT/2;
-    	int horizcent = JFRAME_LENGTH/2;
-    	if((e.getX()<512) && (Math.abs(e.getY()-vertcent)<256)){
-    		onCookieClick();
-    	}
-    }
+       runGame();
+   }
 
-    public void onCookieClick() {
-    	System.out.println("COOKIE CLICKED!");
-    	score += (baseScore* score_multiplier);
+	public static void runGame() {
+		while(true) {
+			updateText(ccta);
+		}
+	}
 
-    }
+	public static void updateText(JTextArea ta) {
+		ta.setText("Cookies: " + score);
+		frame.add(ta);
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		int vertcent = JFRAME_HEIGHT/2;
+		int horizcent = JFRAME_LENGTH/2;
+		if((e.getX()<512) && (Math.abs(e.getY()-vertcent)<256)){
+		cookieClicked();
+		}
+	}
+
+	public void cookieClicked() {
+   	System.out.println("COOKIE CLICKED!");
+   	score += score_multiplier;
+   	}
+
+
+	public int getScore() {
+		return score;
+	}
+
+	public int getScoreMult() {
+		return score_multiplier;
+	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
